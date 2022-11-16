e42882ed6b4d6dd83ec395992476889602cbfad3
Musigwa <pacifique.musigwa@gmail.com>
2022-11-16 10:31:30 +0200

    Add the pull request templates

--- store/slices/authSlice.ts
+++ store/slices/authSlice.ts
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-import { authApi } from "../api-queries/auth-queries";
-import { fetchUsers } from "../api-thunks/auth-thunks";
+import { authApi } from '../api-queries/auth-queries';
+import { fetchUsers } from '../api-thunks/auth-thunks';
 
 export interface IAuthState {
   token: string;
@@ -14,32 +14,32 @@ export interface IAuthState {
 }
 
 const initialState: IAuthState = {
-  token: "",
+  token: '',
   user: null,
-  pushToken: "",
+  pushToken: '',
   loading: false,
   userList: [],
-  error: "",
+  error: '',
   completedOnboarding: false,
 };
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: initialState,
+  name: 'auth',
+  initialState,
   reducers: {
     loggedOut: () => initialState,
     completedOnboarding: (state, action) => {
       state.completedOnboarding = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(fetchUsers.pending, (state) => {
+  extraReducers: builder => {
+    builder.addCase(fetchUsers.pending, state => {
       state.loading = true;
     });
 
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
       //   state.userList = action.payload;
-      console.log("payload", action.payload);
+      console.log('payload', action.payload);
       state.loading = false;
     });
 
@@ -48,21 +48,15 @@ const authSlice = createSlice({
       state.error = action.payload as string;
     });
 
-    builder.addMatcher(
-      authApi.endpoints.signup.matchFulfilled,
-      (state, { payload }) => {
-        state.user = payload.data;
-        state.token = payload.token;
-      }
-    );
+    builder.addMatcher(authApi.endpoints.signup.matchFulfilled, (state, { payload }) => {
+      state.user = payload.data;
+      state.token = payload.token;
+    });
 
-    builder.addMatcher(
-      authApi.endpoints.login.matchFulfilled,
-      (state, { payload }) => {
-        state.user = payload.data;
-        state.token = payload.token;
-      }
-    );
+    builder.addMatcher(authApi.endpoints.login.matchFulfilled, (state, { payload }) => {
+      state.user = payload.data;
+      state.token = payload.token;
+    });
 
     builder.addMatcher(
       authApi.endpoints.uploadProfileDetails.matchFulfilled,
