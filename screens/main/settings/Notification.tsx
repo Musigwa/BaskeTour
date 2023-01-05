97c7e8113afa7d1873b4c5e360e0dda54eea1b21
Musigwa Pacifique <68857067+Musigwa@users.noreply.github.com>
2023-01-06 00:02:20 +0200

    Implement the notification screen design (#27)

--- screens/main/settings/Notification.tsx
+++ screens/main/settings/Notification.tsx
@@ -1,17 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { Horizontal } from '../../../styles/styled-elements';
+import { useTheme } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { Switch, View } from 'react-native';
+import { H4, Horizontal, Separator } from '../../../styles/styled-elements';
 
 const NotificationScreen = () => {
+  const { colors } = useTheme();
+  const [options, setOptions] = useState({
+    General: false,
+    'New chat messages': true,
+    'Get picks in reminder': false,
+    'New round is open': true,
+    'Add more spots to group': true,
+    'Your pick won/lost': false,
+  });
+
+  const handleToggle = optionName => {
+    setOptions(state => ({ ...state, [optionName]: !state[optionName] }));
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
-      <Horizontal>
-        <Text>NotificationScreen</Text>
-      </Horizontal>
+    <View style={{ flex: 1, paddingHorizontal: 24 }}>
+      <View style={{ paddingVertical: 10 }} />
+      <Separator />
+      {Object.keys(options).map((op, idx) => {
+        return (
+          <View key={idx}>
+            <Horizontal style={{ paddingVertical: 32 }}>
+              <H4>{op}</H4>
+              <Switch
+                trackColor={{ true: colors.primary }}
+                value={options[op]}
+                onChange={() => handleToggle(op)}
+              />
+            </Horizontal>
+            <Separator />
+          </View>
+        );
+      })}
     </View>
   );
 };
 
 export default NotificationScreen;
-
-const styles = StyleSheet.create({});
