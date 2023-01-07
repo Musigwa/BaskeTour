a0192db5309f108a3fe7b52a09ba99ea18dfbca7
Musigwa <pacifique.musigwa@gmail.com>
2023-01-07 16:02:24 +0200

    Add pull to reflesh logic

--- components/common/Lists/SearchPaginated.tsx
+++ components/common/Lists/SearchPaginated.tsx
@@ -1,34 +1,47 @@
-import { Entypo, Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Feather, Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 import _ from 'lodash';
-import React, { FC, PropsWithChildren, useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  FC,
+  PropsWithChildren,
+  ReactElement,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import {
   ActivityIndicator,
   FlatList,
-  Image,
-  Pressable,
+  ListRenderItem,
   StyleSheet,
   TextInput,
-  TouchableOpacity,
   View,
 } from 'react-native';
-import { useAppSelector } from '../../../hooks/useStore';
-import { useGetMyGroupsQuery } from '../../../store/api-queries/group-queries';
-import { H3, H5, H6, Horizontal } from '../../../styles/styled-elements';
+import { H5, Horizontal } from '../../../styles/styled-elements';
 
-export const SearchPaginated: FC<
-  PropsWithChildren<{ searchable?: boolean; data: any[]; navigation: any }>
-> = ({ navigation, searchable = true }) => {
+type SearchPaginatedProps = {
+  searchable?: boolean;
+  fetchQuery: Function;
+  params: { [key: string]: any };
+  ListFooterComponent: ReactElement;
+  ListEmptyComponent?: ReactElement;
+  renderItem: ListRenderItem<any>;
+};
+
+export const SearchPaginated: FC<PropsWithChildren<SearchPaginatedProps>> = ({
+  searchable = true,
+  fetchQuery,
+  params,
+  renderItem,
+  ListFooterComponent,
+  ListEmptyComponent = DefaultEmpty,
+}) => {
   const { colors } = useTheme();
-  const { id: userId } = useAppSelector(state => state.auth.user);
   const [searchQuery, setText] = useState<string>('');
   const searchInput = useRef(null);
 
-  const {
-    isFetching,
-    refetch,
-    data: { data: groups = [] } = {},
-  } = useGetMyGroupsQuery({ searchQuery, userId });
+  const { isFetching, refetch, data: { data = [] } = {} } = fetchQuery({ searchQuery, ...params });
   const handleDebTextChange = useMemo(() => _.debounce(setText, 300), []);
 
   useEffect(() => {
@@ -67,85 +80,26 @@ export const SearchPaginated: FC<
       ) : null}
       <FlatList
         refreshing={isFetching}
-        // refreshControl={<ActivityIndicator size='small' color={colors.gray} />}
+        onRefresh={refetch}
         contentContainerStyle={{ flexGrow: 1 }}
-        data={groups}
+        data={data}
         // ListHeaderComponent={<H5>Musigwa</H5>}
         onEndReachedThreshold={0.2}
         // onEndReached={fetchMoreData}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item: group, index: idx }) => {
-          const showCount = 5;
-          const { length } = group.players;
-          const lastIdx = length < showCount ? length : showCount;
-          return (
-            <Pressable
-              style={styles.card}
-              key={idx}
-              onPress={() => navigation.navigate('GroupDetails', { group })}
-            >
-              <H6 style={{ color: group.availableSpots ? colors.gray : colors.primary }}>
-                {`${group.players.length} member${group.players.length > 1 ? 's' : ''}${
-                  !group.availableSpots ? `, ${group.availableSpots} available spots` : ''
-                }`}
-              </H6>
-              <Horizontal>
-                <H3>{group.groupName}</H3>
-                <Entypo name='chevron-small-right' size={24} color={'black'} />
-              </Horizontal>
-              <Horizontal style={{ justifyContent: 'flex-start', marginTop: 5 }}>
-                {group.players.slice(0, showCount).map((player, idx) => {
-                  return (
-                    <View style={[styles.avatarContainer, { left: -(20 * idx) }]} key={idx}>
-                      {player.profilePic ? (
-                        <Image
-                          source={{ uri: player.profilePic }}
-                          style={[styles.image, { backgroundColor: colors.gray }]}
-                          resizeMode='cover'
-                        />
-                      ) : (
-                        <View style={[styles.noImage, { borderColor: colors.gray }]}>
-                          <MaterialIcons name='no-photography' size={24} color={colors.gray} />
-                        </View>
-                      )}
-                    </View>
-                  );
-                })}
-                {lastIdx !== length ? (
-                  <View
-                    style={[styles.extra, { backgroundColor: colors.gray, left: -(20 * lastIdx) }]}
-                  >
-                    <H3 style={{ color: 'white', fontWeight: 'bold' }}>+{length - showCount}</H3>
-                  </View>
-                ) : null}
-              </Horizontal>
-            </Pressable>
-          );
-        }}
-        ListFooterComponent={
-          <View style={{ marginVertical: 80 }}>
-            <TouchableOpacity
-              style={[styles.button, { backgroundColor: colors.primary }]}
-              activeOpacity={0.9}
-              onPress={() => navigation.navigate('CreateGroup')}
-            >
-              <H3 style={{ color: 'white' }}>Create Group</H3>
-            </TouchableOpacity>
-            <H5>
-              The first 5 players in your group are provided for free. If more players want to join,
-              the Group Admin can purchase additional spots for $2 per player.
-            </H5>
-          </View>
-        }
-        ListEmptyComponent={
-          <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
-            <H5>No data matching your search to display!</H5>
-          </View>
-        }
+        renderItem={renderItem}
+        ListFooterComponent={ListFooterComponent}
+        ListEmptyComponent={ListEmptyComponent}
       />
     </View>
   );
 };
+
+const DefaultEmpty = () => (
+  <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
+    <H5>No data matching your search to display!</H5>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 24, backgroundColor: 'rgba(0,0,0,0.02)' },
@@ -158,45 +112,6 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   input: { height: '100%', fontSize: 16, fontFamily: 'Poppins_500Medium' },
-  card: {
-    width: '100%',
-    borderRadius: 4,
-    backgroundColor: 'white',
-    marginBottom: 21,
-    marginTop: 10,
-    shadowColor: 'rgba(35, 20, 115, 0.08)',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.44,
-    shadowRadius: 10.32,
-    elevation: 16,
-    paddingVertical: 15,
-    paddingHorizontal: 24,
-  },
-  button: {
-    paddingVertical: 17,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 6,
-    marginBottom: 30,
-  },
-  extra: {
-    width: 52,
-    height: 52,
-    borderRadius: 26,
-    padding: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  avatarContainer: { borderRadius: 30, backgroundColor: 'white', padding: 2 },
-  image: { width: 50, height: 50, borderRadius: 25 },
-  noImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    borderWidth: StyleSheet.hairlineWidth,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 export default SearchPaginated;
--- screens/main/groups/List.tsx
+++ screens/main/groups/List.tsx
@@ -1,196 +1,141 @@
-// import { Entypo, Feather, MaterialIcons } from '@expo/vector-icons';
-// import { useTheme } from '@react-navigation/native';
-// import _ from 'lodash';
-// import React, { useEffect, useMemo, useState } from 'react';
-// import {
-//   ActivityIndicator,
-//   Image,
-//   Pressable,
-//   ScrollView,
-//   StyleSheet,
-//   TextInput,
-//   TouchableOpacity,
-//   View,
-// } from 'react-native';
-// import { useAppSelector } from '../../../hooks/useStore';
-// import { useGetMyGroupsQuery } from '../../../store/api-queries/group-queries';
-// import { H3, H5, H6, Horizontal } from '../../../styles/styled-elements';
-
-// export const GroupListScreen = ({ navigation }) => {
-//   const { colors } = useTheme();
-//   const { id: userId } = useAppSelector(state => state.auth.user);
-//   const [searchQuery, setText] = useState<string>('');
-//   const {
-//     isFetching,
-//     refetch,
-//     data: { data: groups = [] } = {},
-//   } = useGetMyGroupsQuery({ searchQuery, userId });
-
-//   const handleDebTextChange = useMemo(() => _.debounce(setText, 300), []);
-
-//   useEffect(() => {
-//     refetch();
-//     return handleDebTextChange.cancel;
-//   }, [searchQuery]);
-
-//   return (
-//     <View style={styles.container}>
-//       <Horizontal style={styles.inputWrapper}>
-//         <Feather name='search' size={20} color='black' />
-//         <TextInput
-//           style={{ width: isFetching ? '89%' : '95%', height: '100%', paddingLeft: 10 }}
-//           onChangeText={handleDebTextChange}
-//         />
-//         {isFetching ? <ActivityIndicator size='small' color={colors.gray} /> : null}
-//       </Horizontal>
-
-//       <ScrollView showsVerticalScrollIndicator={false}>
-//         {groups && groups.length
-//           ? groups?.map((group, idx) => {
-//               const showCount = 5;
-//               const { length } = group.players;
-//               const lastIdx = length < showCount ? length : showCount;
-//               return (
-//                 <Pressable
-//                   style={styles.card}
-//                   key={idx}
-//                   onPress={() => navigation.navigate('GroupDetails', { group })}
-//                 >
-//                   <H6 style={{ color: group.availableSpots ? colors.gray : colors.primary }}>
-//                     {`${group.players.length} member${group.players.length > 1 ? 's' : ''}${
-//                       !group.availableSpots ? `, ${group.availableSpots} available spots` : ''
-//                     }`}
-//                   </H6>
-//                   <Horizontal>
-//                     <H3>{group.groupName}</H3>
-//                     <Entypo name='chevron-small-right' size={24} color={'black'} />
-//                   </Horizontal>
-//                   <Horizontal style={{ justifyContent: 'flex-start', marginTop: 5 }}>
-//                     {group.players.slice(0, showCount).map((player, idx) => {
-//                       return (
-//                         <View
-//                           style={{
-//                             borderRadius: 30,
-//                             backgroundColor: 'white',
-//                             padding: 2,
-//                             left: -(20 * idx),
-//                           }}
-//                           key={idx}
-//                         >
-//                           {player.profilePic ? (
-//                             <Image
-//                               source={{ uri: player.profilePic }}
-//                               style={[styles.image, { backgroundColor: colors.gray }]}
-//                               resizeMode='cover'
-//                             />
-//                           ) : (
-//                             <View style={[styles.noImage, { borderColor: colors.gray }]}>
-//                               <MaterialIcons name='no-photography' size={24} color={colors.gray} />
-//                             </View>
-//                           )}
-//                         </View>
-//                       );
-//                     })}
-//                     {lastIdx !== length ? (
-//                       <View
-//                         style={[
-//                           styles.extra,
-//                           { backgroundColor: colors.gray, left: -(20 * lastIdx) },
-//                         ]}
-//                       >
-//                         <H3 style={{ color: 'white', fontWeight: 'bold' }}>
-//                           +{length - showCount}
-//                         </H3>
-//                       </View>
-//                     ) : null}
-//                   </Horizontal>
-//                 </Pressable>
-//               );
-//             })
-//           : null}
-//         <View style={{ marginTop: 150 }}>
-//           <TouchableOpacity
-//             style={[styles.button, { backgroundColor: colors.primary }]}
-//             activeOpacity={0.9}
-//             onPress={() => navigation.navigate('CreateGroup')}
-//           >
-//             <H3 style={{ color: 'white' }}>Create Group</H3>
-//           </TouchableOpacity>
-//           <H5>
-//             The first 5 players in your group are provided for free. If more players want to join,
-//             the Group Admin can purchase additional spots for $2 per player.
-//           </H5>
-//         </View>
-//       </ScrollView>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 24,
-//     backgroundColor: 'rgba(0,0,0,0.02)',
-//   },
-//   inputWrapper: {
-//     height: 48,
-//     backgroundColor: 'rgba(241, 243, 245, 1)',
-//     paddingHorizontal: 15,
-//     borderRadius: 6,
-//     marginBottom: 15,
-//   },
-//   card: {
-//     width: '100%',
-//     borderRadius: 4,
-//     backgroundColor: 'white',
-//     marginBottom: 31,
-//     shadowColor: 'rgba(35, 20, 115, 0.08)',
-//     shadowOffset: {
-//       width: 0,
-//       height: 8,
-//     },
-//     shadowOpacity: 0.44,
-//     shadowRadius: 10.32,
-//     elevation: 16,
-//     paddingVertical: 15,
-//     paddingHorizontal: 24,
-//   },
-//   button: {
-//     paddingVertical: 17,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     borderRadius: 6,
-//     marginBottom: 30,
-//   },
-//   extra: {
-//     width: 52,
-//     height: 52,
-//     borderRadius: 26,
-//     padding: 2,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   image: {
-//     width: 50,
-//     height: 50,
-//     borderRadius: 25,
-//   },
-//   noImage: {
-//     width: 50,
-//     height: 50,
-//     borderRadius: 25,
-//     borderWidth: StyleSheet.hairlineWidth,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-import SearchPaginated from '../../../components/common/Lists/SearchPaginated';
+import { Entypo, MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from '@react-navigation/native';
 import React from 'react';
+import { Image, Pressable, StyleSheet, TouchableOpacity, View } from 'react-native';
+import SearchPaginated from '../../../components/common/Lists/SearchPaginated';
+import { useAppSelector } from '../../../hooks/useStore';
+import { useGetMyGroupsQuery } from '../../../store/api-queries/group-queries';
+import { H3, H5, H6, Horizontal } from '../../../styles/styled-elements';
+
+const listFooterComponent = ({ navigation, colors }) => {
+  return (
+    <View style={{ marginVertical: 80 }}>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: colors.primary }]}
+        activeOpacity={0.9}
+        onPress={() => navigation.navigate('CreateGroup')}
+      >
+        <H3 style={{ color: 'white' }}>Create Group</H3>
+      </TouchableOpacity>
+      <H5>
+        The first 5 players in your group are provided for free. If more players want to join, the
+        Group Admin can purchase additional spots for $2 per player.
+      </H5>
+    </View>
+  );
+};
+
+const renderItem = ({ item: group, index: idx, colors, navigation }) => {
+  const showCount = 5;
+  const { length } = group.players;
+  const lastIdx = length < showCount ? length : showCount;
+  return (
+    <Pressable
+      style={styles.card}
+      key={idx}
+      onPress={() => navigation.navigate('GroupDetails', { group })}
+    >
+      <H6 style={{ color: group.availableSpots ? colors.gray : colors.primary }}>
+        {`${group.players.length} member${group.players.length > 1 ? 's' : ''}${
+          !group.availableSpots ? `, ${group.availableSpots} available spots` : ''
+        }`}
+      </H6>
+      <Horizontal>
+        <H3>{group.groupName}</H3>
+        <Entypo name='chevron-small-right' size={24} color={'black'} />
+      </Horizontal>
+      <Horizontal style={{ justifyContent: 'flex-start', marginTop: 5 }}>
+        {group.players.slice(0, showCount).map((player, idx) => {
+          return (
+            <View style={[styles.avatarContainer, { left: -(20 * idx) }]} key={idx}>
+              {player.profilePic ? (
+                <Image
+                  source={{ uri: player.profilePic }}
+                  style={[styles.image, { backgroundColor: colors.gray }]}
+                  resizeMode='cover'
+                />
+              ) : (
+                <View style={[styles.noImage, { borderColor: colors.gray }]}>
+                  <MaterialIcons name='no-photography' size={24} color={colors.gray} />
+                </View>
+              )}
+            </View>
+          );
+        })}
+        {lastIdx !== length ? (
+          <View style={[styles.extra, { backgroundColor: colors.gray, left: -(20 * lastIdx) }]}>
+            <H3 style={{ color: 'white', fontWeight: 'bold' }}>+{length - showCount}</H3>
+          </View>
+        ) : null}
+      </Horizontal>
+    </Pressable>
+  );
+};
 
 export const GroupListScreen = ({ navigation }) => {
-  const groups = [];
-  return <SearchPaginated navigation={navigation} data={groups} />;
+  const { id: userId } = useAppSelector(state => state.auth.user);
+  const { colors } = useTheme();
+
+  return (
+    <SearchPaginated
+      fetchQuery={useGetMyGroupsQuery}
+      params={{ userId }}
+      ListFooterComponent={listFooterComponent({ navigation, colors })}
+      renderItem={args => renderItem({ ...args, navigation, colors })}
+    />
+  );
 };
 
 export default GroupListScreen;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 24, backgroundColor: 'rgba(0,0,0,0.02)' },
+  inputWrapper: {
+    height: 48,
+    backgroundColor: 'rgba(241, 243, 245, 1)',
+    paddingHorizontal: 10,
+    borderRadius: 6,
+    marginBottom: 15,
+    width: '100%',
+  },
+  input: { height: '100%', fontSize: 16, fontFamily: 'Poppins_500Medium' },
+  card: {
+    width: '100%',
+    borderRadius: 4,
+    backgroundColor: 'white',
+    marginBottom: 21,
+    marginTop: 10,
+    shadowColor: 'rgba(35, 20, 115, 0.08)',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
+    elevation: 16,
+    paddingVertical: 15,
+    paddingHorizontal: 24,
+  },
+  button: {
+    paddingVertical: 17,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+    marginBottom: 30,
+  },
+  extra: {
+    width: 52,
+    height: 52,
+    borderRadius: 26,
+    padding: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatarContainer: { borderRadius: 30, backgroundColor: 'white', padding: 2 },
+  image: { width: 50, height: 50, borderRadius: 25 },
+  noImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: StyleSheet.hairlineWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
